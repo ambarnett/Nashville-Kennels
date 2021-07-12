9bf12a59a691c9c68eb287862b881a8d815ce1ff
ambarnett <andy.barnett09@gmail.com>
2021-07-12 16:28:31 -0500

    added detailed animal view

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -9,32 +9,34 @@ import "./Animal.css"
 export const AnimalList = () => {
     // This state changes when `getAnimals()` is invoked below
     const { animals, getAnimals } = useContext(AnimalContext)
-    const { locations, getLocations } = useContext(LocationContext)
-    const { customers, getCustomers } = useContext(CustomerContext)
+    // const { locations, getLocations } = useContext(LocationContext)
+    // const { customers, getCustomers } = useContext(CustomerContext)
     const history = useHistory()
 
     //useEffect - reach out to the world for something
     useEffect(() => {
         console.log("AnimalsList: Initial render before data")
-        getLocations()
-        .then(getCustomers)
-        .then(getAnimals)
+        getAnimals()
+        // getLocations()
+        // .then(getCustomers)
+        // .then(getAnimals)
 
     }, [])// The empty square brackets are there to say only call the component once; when it first renders
     return (
         <>
             <h2>Animals</h2>
             <button onClick={() => { history.push("/animals/create") }}>
-                Add Animal
+                Make Reservation
             </button>
             <div className="animals">
                 {console.log("AnimalList: Render", animals)}
                 {
                     animals.map(animal => {
-                        const owner = customers.find(c => c.id === animal.customerId)
-                        const clinic = locations.find(l => l.id === animal.locationId)
+                        // const owner = customers.find(c => c.id === animal.customerId)
+                        // const clinic = locations.find(l => l.id === animal.locationId)
 
-                        return <AnimalCard key={animal.id} location={clinic} customer={owner} animal={animal} />
+                        // return <AnimalCard key={animal.id} location={clinic} customer={owner} animal={animal} />
+                        return <AnimalCard key={animal.id} animal={animal} />
                     })
                 }
             </div>
--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -23,6 +23,11 @@ export const AnimalProvider = (props) => {
         })
         .then(res => res.json())
     }
+
+    const getAnimalById = (id) => {
+        return fetch(`http://localhost:8088/animals/${id}?_expand=location&_expand=customer`)
+        .then(res => res.json())
+    }
     /*
         You return a context provider which has the
         `animals` state, `getAnimals` function,
@@ -31,7 +36,7 @@ export const AnimalProvider = (props) => {
     */
    return (
        <AnimalContext.Provider value={{
-           animals, getAnimals, addAnimal
+           animals, getAnimals, addAnimal, getAnimalById
        }}>
            {props.children}
        </AnimalContext.Provider>
